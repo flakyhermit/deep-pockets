9b6373ef6814ed33b15e4ee164a8da4fa17f5a74
Jewel James <flakyhermit@protonmail.ch>
2020-11-24 13:01:57 +0530

    Fixed header

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -4,9 +4,8 @@
     <div id="header-title" class="has-text-primary is-size-4 has-text-weight-bold">{{ title }}</div>
     <div id="header-description" class="has-text-grey">{{ description }}</div>
   </div> -->
-  <div class="px-5 py-5 is-flex is-justify-content-space-evenly">
-    <div class="has-text-primary is-flex-grow-1 has-text-weight-bold is-size-4 mr-5">{{ title }}</div>
-    <div class="has-color-grey is-size-6">{{ description }}</div>
+  <div class="is-flex m-3">
+    <div class="is-flex-grow-1 has-text-centered has-text-primary is-size-4"><b>{{ title }}</b></div>
   </div>
 </template>
 
